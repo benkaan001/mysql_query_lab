8891f5d13d8730b973e0d70bf98704a45abbf3a2
benkaan001 <benkaan001@gmail.com>
2023-05-13 23:21:41 -0500

    update table columns

--- mysql_query_lab/databases/py_brew_db/create_tables.sql
+++ mysql_query_lab/databases/py_brew_db/create_tables.sql
@@ -1,16 +1,6 @@
-CREATE TABLE IF NOT EXISTS fact_sales (
-    sales_id INT PRIMARY KEY,
-    date_id INT NOT NULL,
-    product_id INT NOT NULL,
-    customer_id INT NOT NULL,
-    sales_amount DECIMAL(10,2) NOT NULL,
-    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
-    FOREIGN KEY (product_id) REFERENCES dim_product(product_id),
-    FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id)
-);
-
 
 CREATE TABLE IF NOT EXISTS dim_date (
+    tmstp TIMESTAMP NOT NULL,
     date_id INT PRIMARY KEY,
     date DATE NOT NULL,
     day_of_week VARCHAR(10) NOT NULL,
@@ -19,12 +9,12 @@ CREATE TABLE IF NOT EXISTS dim_date (
 );
 
 
-
 CREATE TABLE IF NOT EXISTS dim_product (
-    product_id INT PRIMARY KEY,
+    product_id VARCHAR(20) PRIMARY KEY,
     product_name VARCHAR(50) NOT NULL,
     product_category VARCHAR(50) NOT NULL,
-    product_subcategory VARCHAR(50) NOT NULL
+    product_subcategory VARCHAR(50) NOT NULL,
+    product_price DECIMAL(4,2) NOT NULL
 );
 
 
@@ -36,6 +26,16 @@ CREATE TABLE IF NOT EXISTS dim_customer (
     customer_state VARCHAR(50) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS fact_sales (
+    sales_id INT PRIMARY KEY,
+    date_id INT NOT NULL,
+    product_id VARCHAR(20) NOT NULL,
+    customer_id INT NOT NULL,
+    sales_amount DECIMAL(10,2) NOT NULL,
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (product_id) REFERENCES dim_product(product_id),
+    FOREIGN KEY (customer_id) REFERENCES dim_customer(customer_id)
+);
 
 
 
