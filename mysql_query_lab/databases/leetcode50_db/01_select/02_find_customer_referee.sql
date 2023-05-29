5a762ce5a9b33dd738664559098e6d1c9de9ab84
benkaan001 <benkaan001@gmail.com>
2023-05-29 17:01:49 -0500

    update tests and add widget

---
+++ mysql_query_lab/databases/leetcode50_db/01_select/02_find_customer_referee.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Customer (
+  id INT,
+  name VARCHAR(25),
+  referee_id INT
+);
+
+TRUNCATE TABLE Customer;
+
+INSERT INTO Customer (id, name, referee_id) VALUES
+  (1, 'Will', NULL),
+  (2, 'Jane', NULL),
+  (3, 'Alex', 2),
+  (4, 'Bill', NULL),
+  (5, 'Zack', 1),
+  (6, 'Mark', 2);
