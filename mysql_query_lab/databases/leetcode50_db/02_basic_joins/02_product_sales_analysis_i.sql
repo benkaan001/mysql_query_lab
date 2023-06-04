16089436a1ea3456d1d19953edd956fea1a7e717
benkaan001 <benkaan001@gmail.com>
2023-06-04 11:56:01 -0500

    update 2-2 table names

--- mysql_query_lab/databases/leetcode50_db/02_basic_joins/02_product_sales_analysis_i.sql
+++ mysql_query_lab/databases/leetcode50_db/02_basic_joins/02_product_sales_analysis_i.sql
@@ -1,4 +1,7 @@
-CREATE TABLE IF NOT EXISTS Sales2 (
+DROP TABLE IF EXISTS Sales;
+DROP TABLE IF EXISTS Product;
+
+CREATE TABLE IF NOT EXISTS Sales (
   sale_id INT,
   product_id INT,
   year INT,
@@ -6,19 +9,19 @@ CREATE TABLE IF NOT EXISTS Sales2 (
   price INT
 );
 
-CREATE TABLE IF NOT EXISTS Product2 (
+CREATE TABLE IF NOT EXISTS Product (
   product_id INT,
   product_name VARCHAR(10)
 );
 
-TRUNCATE TABLE Sales2;
+TRUNCATE TABLE Sales;
 
-INSERT INTO Sales2 (sale_id, product_id, year, quantity, price) VALUES (1, 100, 2008, 10, 5000);
-INSERT INTO Sales2 (sale_id, product_id, year, quantity, price) VALUES (2, 100, 2009, 12, 5000);
-INSERT INTO Sales2 (sale_id, product_id, year, quantity, price) VALUES (7, 200, 2011, 15, 9000);
+INSERT INTO Sales (sale_id, product_id, year, quantity, price) VALUES (1, 100, 2008, 10, 5000);
+INSERT INTO Sales (sale_id, product_id, year, quantity, price) VALUES (2, 100, 2009, 12, 5000);
+INSERT INTO Sales (sale_id, product_id, year, quantity, price) VALUES (7, 200, 2011, 15, 9000);
 
-TRUNCATE TABLE Product2;
+TRUNCATE TABLE Product;
 
-INSERT INTO Product2 (product_id, product_name) VALUES (100, 'Nokia');
-INSERT INTO Product2 (product_id, product_name) VALUES (200, 'Apple');
-INSERT INTO Product2 (product_id, product_name) VALUES (300, 'Samsung');
+INSERT INTO Product (product_id, product_name) VALUES (100, 'Nokia');
+INSERT INTO Product (product_id, product_name) VALUES (200, 'Apple');
+INSERT INTO Product (product_id, product_name) VALUES (300, 'Samsung');
